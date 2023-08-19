522449cb85e9eb93c2b195828114802bd15bb26d
Jinfa <54011412+Jinfa@users.noreply.github.com>
2023-08-19 15:03:59 +0800

    Add files via upload

---
+++ tkbc/learner.py
@@ -0,0 +1,168 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+import argparse
+from typing import Dict
+import logging
+import torch
+from torch import optim
+import sys
+
+from datasets import TemporalDataset
+from optimizers import TKBCOptimizer, IKBCOptimizer
+from models import ComplEx, TComplEx, TPComplEx
+from regularizers import N3, Lambda3
+
+parser = argparse.ArgumentParser(
+    description="Temporal ComplEx"
+)
+parser.add_argument(
+    '--dataset',default='ICEWS14' , type=str,
+    help="Dataset name"
+)
+models = [
+    'ComplEx', 'TComplEx', 'TPComplEx'
+]
+parser.add_argument(
+    '--model',default = 'TPComplEx', choices=models,
+    help="Model in {}".format(models)
+)
+parser.add_argument(
+    '--max_epochs', default=50, type=int,
+    help="Number of epochs."
+)
+parser.add_argument(
+    '--valid_freq', default=5, type=int,
+    help="Number of epochs between each valid."
+)
+parser.add_argument(
+    '--rank', default=100, type=int,
+    help="Factorization rank."
+)
+parser.add_argument(
+    '--batch_size', default=1000, type=int,
+    help="Batch size."
+)
+parser.add_argument(
+    '--learning_rate', default=1e-1, type=float,
+    help="Learning rate"
+)
+parser.add_argument(
+    '--emb_reg', default=0., type=float,
+    help="Embedding regularizer strength"
+)
+parser.add_argument(
+    '--time_reg', default=0., type=float,
+    help="Timestamp regularizer strength"
+)
+parser.add_argument(
+    '--no_time_emb', default=False, action="store_true",
+    help="Use a specific embedding for non temporal relations"
+)
+
+parser.add_argument('-test', '--do_test', action='store_true') #action='store_true'
+parser.add_argument('-save', '--do_save', action='store_true')
+parser.add_argument('-id', '--model_id', type=str, default='0')
+
+args = parser.parse_args()
+
+model_path = './logs/' + args.model + '_' + args.dataset  + '_' + args.model_id
+
+dataset = TemporalDataset(args.dataset)
+
+sizes = dataset.get_shape()
+model = {
+    'ComplEx': ComplEx(sizes, args.rank),
+    'TComplEx': TComplEx(sizes, args.rank, no_time_emb=args.no_time_emb),
+    'TPComplEx': TPComplEx(sizes, args.rank, no_time_emb=args.no_time_emb),
+}[args.model]
+model = model.cuda()
+
+
+opt = optim.Adagrad(model.parameters(), lr=args.learning_rate)
+
+emb_reg = N3(args.emb_reg)
+time_reg = Lambda3(args.time_reg)
+
+
+if args.do_test:
+    # testModel = model(config=self)
+
+    def avg_both(mrrs: Dict[str, float], hits: Dict[str, torch.FloatTensor]):
+        """
+        aggregate metrics for missing lhs and rhs
+        :param mrrs: d
+        :param hits:
+        :return:
+        """
+        m = (mrrs['lhs'] + mrrs['rhs']) / 2.
+        h = (hits['lhs'] + hits['rhs']) / 2.
+        return {'MRR': m, 'hits@[1,3,10]': h}
+
+
+    model_dict=torch.load(model_path)
+    valid, test, train = [
+                avg_both(*dataset.eval(model_dict, split, -1 if split != 'train' else 50000))
+                for split in ['valid', 'test', 'train']
+            ]
+    print("valid: ", valid)
+    print("test: ", test)
+    print("train: ", train) 
+    sys.exit(0)
+
+
+
+
+
+for epoch in range(args.max_epochs):
+    examples = torch.from_numpy(
+        dataset.get_train().astype('int64')
+    )
+
+    model.train()
+    if dataset.has_intervals():
+        optimizer = IKBCOptimizer(
+            model, emb_reg, time_reg, opt, dataset,
+            batch_size=args.batch_size
+        )
+        optimizer.epoch(examples)
+
+    else:
+        optimizer = TKBCOptimizer(
+            model, emb_reg, time_reg, opt,
+            batch_size=args.batch_size
+        )
+        optimizer.epoch(examples)
+
+
+    def avg_both(mrrs: Dict[str, float], hits: Dict[str, torch.FloatTensor]):
+        """
+        aggregate metrics for missing lhs and rhs
+        :param mrrs: d
+        :param hits:
+        :return:
+        """
+        m = (mrrs['lhs'] + mrrs['rhs']) / 2.
+        h = (hits['lhs'] + hits['rhs']) / 2.
+        return {'MRR': m, 'hits@[1,3,10]': h}
+
+    if epoch < 0 or (epoch + 1) % args.valid_freq == 0:
+        if dataset.has_intervals():
+            valid, test, train = [
+                dataset.eval(model, split, -1 if split != 'train' else 50000)
+                for split in ['valid', 'test', 'train']
+            ]
+            print("valid: ", valid)
+            print("test: ", test)
+            print("train: ", train)
+
+        else:
+            valid, test, train = [
+                avg_both(*dataset.eval(model, split, -1 if split != 'train' else 50000))
+                for split in ['valid', 'test', 'train']
+            ]
+            print("valid: ", valid)
+            print("test: ", test)
+            print("train: ", train)
+        if args.do_save:
+            torch.save(model, model_path)
+
---
+++ tkbc/models.py
@@ -0,0 +1,432 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from abc import ABC, abstractmethod
+from typing import Tuple, List, Dict
+
+import math
+import torch
+from torch import nn
+import numpy as np
+
+
+class TKBCModel(nn.Module, ABC):
+    @abstractmethod
+    def get_rhs(self, chunk_begin: int, chunk_size: int):
+        pass
+
+    @abstractmethod
+    def get_queries(self, queries: torch.Tensor):
+        pass
+
+    @abstractmethod
+    def score(self, x: torch.Tensor):
+        pass
+
+    @abstractmethod
+    def forward_over_time(self, x: torch.Tensor):
+        pass
+
+    def get_ranking(
+            self, queries: torch.Tensor,
+            filters: Dict[Tuple[int, int, int], List[int]],
+            batch_size: int = 1000, chunk_size: int = -1
+    ):
+        """
+        Returns filtered ranking for each queries.
+        :param queries: a torch.LongTensor of quadruples (lhs, rel, rhs, timestamp)
+        :param filters: filters[(lhs, rel, ts)] gives the elements to filter from ranking
+        :param batch_size: maximum number of queries processed at once
+        :param chunk_size: maximum number of candidates processed at once
+        :return:
+        """
+        if chunk_size < 0:
+            chunk_size = self.sizes[2]
+        ranks = torch.ones(len(queries))
+        with torch.no_grad():
+            c_begin = 0
+            while c_begin < self.sizes[2]:
+                b_begin = 0
+                rhs = self.get_rhs(c_begin, chunk_size)
+                while b_begin < len(queries):
+                    these_queries = queries[b_begin:b_begin + batch_size]
+                    q = self.get_queries(these_queries)
+
+                    scores = q @ rhs
+                    targets = self.score(these_queries)
+                    assert not torch.any(torch.isinf(scores)), "inf scores"
+                    assert not torch.any(torch.isnan(scores)), "nan scores"
+                    assert not torch.any(torch.isinf(targets)), "inf targets"
+                    assert not torch.any(torch.isnan(targets)), "nan targets"
+
+                    # set filtered and true scores to -1e6 to be ignored
+                    # take care that scores are chunked
+                    for i, query in enumerate(these_queries):
+                        filter_out = filters[(query[0].item(), query[1].item(), query[3].item())]
+                        filter_out += [queries[b_begin + i, 2].item()]
+                        if chunk_size < self.sizes[2]:
+                            filter_in_chunk = [
+                                int(x - c_begin) for x in filter_out
+                                if c_begin <= x < c_begin + chunk_size
+                            ]
+                            scores[i, torch.LongTensor(filter_in_chunk)] = -1e6
+                        else:
+                            scores[i, torch.LongTensor(filter_out)] = -1e6
+                    ranks[b_begin:b_begin + batch_size] += torch.sum(
+                        (scores >= targets).float(), dim=1
+                    ).cpu()
+
+                    b_begin += batch_size
+
+                c_begin += chunk_size
+        return ranks
+
+    def get_auc(
+            self, queries: torch.Tensor, batch_size: int = 1000
+    ):
+        """
+        Returns filtered ranking for each queries.
+        :param queries: a torch.LongTensor of quadruples (lhs, rel, rhs, begin, end)
+        :param batch_size: maximum number of queries processed at once
+        :return:
+        """
+        all_scores, all_truth = [], []
+        all_ts_ids = None
+        with torch.no_grad():
+            b_begin = 0
+            while b_begin < len(queries):
+                these_queries = queries[b_begin:b_begin + batch_size]
+                scores = self.forward_over_time(these_queries)
+                all_scores.append(scores.cpu().numpy())
+                if all_ts_ids is None:
+                    all_ts_ids = torch.arange(0, scores.shape[1]).cuda()[None, :]
+                assert not torch.any(torch.isinf(scores) + torch.isnan(scores)), "inf or nan scores"
+                truth = (all_ts_ids <= these_queries[:, 4][:, None]) * (all_ts_ids >= these_queries[:, 3][:, None])
+                all_truth.append(truth.cpu().numpy())
+                b_begin += batch_size
+
+        return np.concatenate(all_truth), np.concatenate(all_scores)
+
+    def get_time_ranking(
+            self, queries: torch.Tensor, filters: List[List[int]], chunk_size: int = -1
+    ):
+        """
+        Returns filtered ranking for a batch of queries ordered by timestamp.
+        :param queries: a torch.LongTensor of quadruples (lhs, rel, rhs, timestamp)
+        :param filters: ordered filters
+        :param chunk_size: maximum number of candidates processed at once
+        :return:
+        """
+        if chunk_size < 0:
+            chunk_size = self.sizes[2]
+        ranks = torch.ones(len(queries))
+        with torch.no_grad():
+            c_begin = 0
+            q = self.get_queries(queries)
+            targets = self.score(queries)
+            while c_begin < self.sizes[2]:
+                rhs = self.get_rhs(c_begin, chunk_size)
+                scores = q @ rhs
+                # set filtered and true scores to -1e6 to be ignored
+                # take care that scores are chunked
+                for i, (query, filter) in enumerate(zip(queries, filters)):
+                    filter_out = filter + [query[2].item()]
+                    if chunk_size < self.sizes[2]:
+                        filter_in_chunk = [
+                            int(x - c_begin) for x in filter_out
+                            if c_begin <= x < c_begin + chunk_size
+                        ]
+                        max_to_filter = max(filter_in_chunk + [-1])
+                        assert max_to_filter < scores.shape[1], f"fuck {scores.shape[1]} {max_to_filter}"
+                        scores[i, filter_in_chunk] = -1e6
+                    else:
+                        scores[i, filter_out] = -1e6
+                ranks += torch.sum(
+                    (scores >= targets).float(), dim=1
+                ).cpu()
+
+                c_begin += chunk_size
+        return ranks
+
+
+
+class ComplEx(TKBCModel):
+    def __init__(
+            self, sizes: Tuple[int, int, int, int], rank: int,
+            init_size: float = 1e-3
+    ):
+        super(ComplEx, self).__init__()
+        self.sizes = sizes
+        self.rank = rank
+
+        self.embeddings = nn.ModuleList([
+            nn.Embedding(s, 2 * rank, sparse=True)
+            for s in [sizes[0], sizes[1]]
+        ])
+        self.embeddings[0].weight.data *= init_size
+        self.embeddings[1].weight.data *= init_size
+
+    @staticmethod
+    def has_time():
+        return False
+
+    def forward_over_time(self, x):
+        raise NotImplementedError("no.")
+
+    def score(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+
+        return torch.sum(
+            (lhs[0] * rel[0] - lhs[1] * rel[1]) * rhs[0] +
+            (lhs[0] * rel[1] + lhs[1] * rel[0]) * rhs[1],
+            1, keepdim=True
+        )
+
+    def forward(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+
+        right = self.embeddings[0].weight
+        right = right[:, :self.rank], right[:, self.rank:]
+        return (
+                       (lhs[0] * rel[0] - lhs[1] * rel[1]) @ right[0].transpose(0, 1) +
+                       (lhs[0] * rel[1] + lhs[1] * rel[0]) @ right[1].transpose(0, 1)
+               ), (
+                   torch.sqrt(lhs[0] ** 2 + lhs[1] ** 2),
+                   torch.sqrt(rel[0] ** 2 + rel[1] ** 2),
+                   torch.sqrt(rhs[0] ** 2 + rhs[1] ** 2)
+               ), None
+
+    def get_rhs(self, chunk_begin: int, chunk_size: int):
+        return self.embeddings[0].weight.data[
+               chunk_begin:chunk_begin + chunk_size
+               ].transpose(0, 1)
+
+    def get_queries(self, queries: torch.Tensor):
+        lhs = self.embeddings[0](queries[:, 0])
+        rel = self.embeddings[1](queries[:, 1])
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+
+        return torch.cat([
+            lhs[0] * rel[0] - lhs[1] * rel[1],
+            lhs[0] * rel[1] + lhs[1] * rel[0]
+        ], 1)
+
+
+class TComplEx(TKBCModel):
+    def __init__(
+            self, sizes: Tuple[int, int, int, int], rank: int,
+            no_time_emb=False, init_size: float = 1e-2
+    ):
+        super(TComplEx, self).__init__()
+        self.sizes = sizes
+        self.rank = rank
+
+        self.embeddings = nn.ModuleList([
+            nn.Embedding(s, 2 * rank, sparse=True)
+            for s in [sizes[0], sizes[1], sizes[3]]
+        ])
+        self.embeddings[0].weight.data *= init_size
+        self.embeddings[1].weight.data *= init_size
+        self.embeddings[2].weight.data *= init_size
+
+        self.no_time_emb = no_time_emb
+
+    @staticmethod
+    def has_time():
+        return True
+
+    def score(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+        time = self.embeddings[2](x[:, 3])
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+        time = time[:, :self.rank], time[:, self.rank:]
+
+        return torch.sum(
+            (lhs[0] * rel[0] * time[0] - lhs[1] * rel[1] * time[0] -
+             lhs[1] * rel[0] * time[1] - lhs[0] * rel[1] * time[1]) * rhs[0] +
+            (lhs[1] * rel[0] * time[0] + lhs[0] * rel[1] * time[0] +
+             lhs[0] * rel[0] * time[1] - lhs[1] * rel[1] * time[1]) * rhs[1],
+            1, keepdim=True
+        )
+
+    def forward(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+        time = self.embeddings[2](x[:, 3])
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+        time = time[:, :self.rank], time[:, self.rank:]
+
+        right = self.embeddings[0].weight
+        right = right[:, :self.rank], right[:, self.rank:]
+
+        rt = rel[0] * time[0], rel[1] * time[0], rel[0] * time[1], rel[1] * time[1]
+        full_rel = rt[0] - rt[3], rt[1] + rt[2]
+
+        return (
+                       (lhs[0] * full_rel[0] - lhs[1] * full_rel[1]) @ right[0].t() +
+                       (lhs[1] * full_rel[0] + lhs[0] * full_rel[1]) @ right[1].t()
+               ), (
+                   torch.sqrt(lhs[0] ** 2 + lhs[1] ** 2),
+                   torch.sqrt(full_rel[0] ** 2 + full_rel[1] ** 2),
+                   torch.sqrt(rhs[0] ** 2 + rhs[1] ** 2)
+               ), self.embeddings[2].weight[:-1] if self.no_time_emb else self.embeddings[2].weight
+
+    def forward_over_time(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+        time = self.embeddings[2].weight
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+        time = time[:, :self.rank], time[:, self.rank:]
+
+        return (
+                (lhs[0] * rel[0] * rhs[0] - lhs[1] * rel[1] * rhs[0] -
+                 lhs[1] * rel[0] * rhs[1] + lhs[0] * rel[1] * rhs[1]) @ time[0].t() +
+                (lhs[1] * rel[0] * rhs[0] - lhs[0] * rel[1] * rhs[0] +
+                 lhs[0] * rel[0] * rhs[1] - lhs[1] * rel[1] * rhs[1]) @ time[1].t()
+        )
+
+    def get_rhs(self, chunk_begin: int, chunk_size: int):
+        return self.embeddings[0].weight.data[
+               chunk_begin:chunk_begin + chunk_size
+               ].transpose(0, 1)
+
+    def get_queries(self, queries: torch.Tensor):
+        lhs = self.embeddings[0](queries[:, 0])
+        rel = self.embeddings[1](queries[:, 1])
+        time = self.embeddings[2](queries[:, 3])
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        time = time[:, :self.rank], time[:, self.rank:]
+        return torch.cat([
+            lhs[0] * rel[0] * time[0] - lhs[1] * rel[1] * time[0] -
+            lhs[1] * rel[0] * time[1] - lhs[0] * rel[1] * time[1],
+            lhs[1] * rel[0] * time[0] + lhs[0] * rel[1] * time[0] +
+            lhs[0] * rel[0] * time[1] - lhs[1] * rel[1] * time[1]
+        ], 1)
+
+
+
+class TPComplEx(TKBCModel):
+    def __init__(
+            self, sizes: Tuple[int, int, int, int], rank: int,
+            no_time_emb=False, init_size: float = 1e-2
+    ):
+        super(TPComplEx, self).__init__()
+        self.sizes = sizes
+        self.rank = rank
+
+        self.embeddings = nn.ModuleList([
+            nn.Embedding(sizes[0], 2 * rank, sparse=True),
+            nn.Embedding(sizes[1], 2 * rank, sparse=True),
+            nn.Embedding(sizes[2], 6 * rank, sparse=True)
+            
+        ])
+        self.embeddings[0].weight.data *= init_size
+        self.embeddings[1].weight.data *= init_size
+        self.embeddings[2].weight.data *= init_size
+
+        self.no_time_emb = no_time_emb
+
+    @staticmethod
+    def has_time():
+        return True
+
+    def score(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+        time = self.embeddings[2](x[:, 3])
+
+        lhs = lhs[:, :self.rank]+time[:, 2*self.rank:3*self.rank], lhs[:, self.rank:]+time[:, 3*self.rank:4*self.rank]
+        rhs = rhs[:, :self.rank]+time[:, 4*self.rank:5*self.rank], rhs[:, self.rank:]+time[:, 5*self.rank:6*self.rank]
+        rel = rel[:, :self.rank], rel[:, self.rank:2*self.rank]
+        
+        time = time[:, :self.rank], time[:, self.rank:2*self.rank]
+
+        return torch.sum(
+            (lhs[0] * rel[0] * time[0] - lhs[1] * rel[1] * time[0] -
+             lhs[1] * rel[0] * time[1] - lhs[0] * rel[1] * time[1]) * rhs[0] +
+            (lhs[1] * rel[0] * time[0] + lhs[0] * rel[1] * time[0] +
+             lhs[0] * rel[0] * time[1] - lhs[1] * rel[1] * time[1]) * rhs[1],
+            1, keepdim=True
+        )
+
+    def forward(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+        time = self.embeddings[2](x[:, 3])
+
+        lhs = lhs[:, :self.rank]+time[:, 2*self.rank:3*self.rank], lhs[:, self.rank:]+time[:, 3*self.rank:4*self.rank]
+        rhs = rhs[:, :self.rank]+time[:, 4*self.rank:5*self.rank], rhs[:, self.rank:]+time[:, 5*self.rank:6*self.rank]
+        bias_t_r = time[:, 4*self.rank:5*self.rank]
+        bias_t_i = time[:, 5*self.rank:6*self.rank]
+        
+        
+        time = time[:, :self.rank], time[:, self.rank:2*self.rank]
+
+        right = self.embeddings[0].weight
+        right = right[:, :self.rank], right[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:2*self.rank]
+
+        rt = rel[0] * time[0], rel[1] * time[0], rel[0] * time[1], rel[1] * time[1]
+        full_rel = rt[0] - rt[3], rt[1] + rt[2]
+
+        return (
+                       (lhs[0] * full_rel[0] - lhs[1] * full_rel[1]) @ right[0].t() + torch.sum((lhs[0] * full_rel[0] - lhs[1] * full_rel[1]) * bias_t_r,1,keepdim=True) +
+                       (lhs[1] * full_rel[0] + lhs[0] * full_rel[1]) @ right[1].t() + torch.sum((lhs[1] * full_rel[0] + lhs[0] * full_rel[1]) * bias_t_i,1,keepdim=True)
+               ), (
+                   torch.sqrt(lhs[0] ** 2 + lhs[1] ** 2),
+                   torch.sqrt(full_rel[0] ** 2 + full_rel[1] ** 2),
+                   torch.sqrt(rhs[0] ** 2 + rhs[1] ** 2)
+               ), self.embeddings[2].weight[:-1] if self.no_time_emb else self.embeddings[2].weight
+
+    def forward_over_time(self, x): 
+        raise NotImplementedError("no.")
+        
+    def get_rhs(self, chunk_begin: int, chunk_size: int):
+        return self.embeddings[0].weight.data[
+               chunk_begin:chunk_begin + chunk_size
+               ].transpose(0, 1)
+
+    def get_queries(self, queries: torch.Tensor):
+        lhs = self.embeddings[0](queries[:, 0])
+        rel = self.embeddings[1](queries[:, 1])
+        time = self.embeddings[2](queries[:, 3])
+        lhs = lhs[:, :self.rank]+time[:, 2*self.rank:3*self.rank], lhs[:, self.rank:]+time[:, 3*self.rank:4*self.rank]
+        rel = rel[:, :self.rank], rel[:, self.rank:2*self.rank]
+        # rhs = rhs[:, :self.rank]+rel[:, 4*self.rank:5*self.rank], rhs[:, self.rank:]+rel[:, 5*self.rank:6*self.rank]
+        time = time[:, :self.rank], time[:, self.rank:2*self.rank]
+        return torch.cat([
+            lhs[0] * rel[0] * time[0] - lhs[1] * rel[1] * time[0] -
+            lhs[1] * rel[0] * time[1] - lhs[0] * rel[1] * time[1],
+            lhs[1] * rel[0] * time[0] + lhs[0] * rel[1] * time[0] +
+            lhs[0] * rel[0] * time[1] - lhs[1] * rel[1] * time[1]
+        ], 1)
+
+
